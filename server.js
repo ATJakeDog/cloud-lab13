ffc33d6f73b8c10c70e4e493ba8bffdb5af9fec9
Владислав Бугаев <bugayev.vlad@gmail.com>
2025-11-29 12:40:35 +0500

    Update to v2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World v1");
+  res.send("Hello World v2 (Updated)");
 });
 
 const port = process.env.PORT || 3000;
